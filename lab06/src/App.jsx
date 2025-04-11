3354befb39bb965c99c863ccb7383ec27b9ce785
HaoLe2710 <lengochao123.27102004@gmail.com>
2025-04-11 23:26:20 +0700

    isActive for status of user and isActive for NavBar using NavLink of react-router-dom

--- lab06/src/App.jsx
+++ lab06/src/App.jsx
@@ -1,17 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Dashboard from './pages/Dashboard'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import Dashboard from './pages/Dashboard';
+import Projects from './pages/Projects';
+import Teams from './pages/Teams';
+import Analytics from './pages/Analytics';
+import Messages from './pages/Messages';
+import Integrations from './pages/Integrations';
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <Dashboard/>
-    </>
-  )
+    <Router>
+      <div className="grid grid-cols-4">
+        <div className='mt-5'><NavBar /></div>
+        <div className="col-span-3 flex-1 p-4">
+          <Routes>
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/projects" element={<Projects/>} />
+            <Route path="/teams" element={<Teams />} />
+            <Route path="/analytics" element={<Analytics />} />
+            <Route path="/messages" element={<Messages />} />
+            <Route path="/integrations" element={<Integrations />} />
+            <Route path="*" element={<Dashboard />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- lab06/src/pages/Dashboard.jsx
+++ lab06/src/pages/Dashboard.jsx
@@ -2,14 +2,9 @@ import DashboardDetail from "../components/DashboardDetail";
 import NavBar from "../components/NavBar";
 
 export default function Dashboard(){
-    return(
-        <div className=" grid grid-cols-4">
-            <div className="bg-amber-200">
-                <NavBar/>
-            </div>
-            <div className="col-span-3 bg-blue-300">
-                <DashboardDetail/>
-            </div>
+    return(        
+        <div className=" bg-blue-300">
+            <DashboardDetail/>
         </div>
     )
 }
