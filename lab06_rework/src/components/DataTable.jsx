1ab86b75f1889ee349e23689f25b5ad6830674a9
HaoLe2710 <lengochao123.27102004@gmail.com>
2025-04-20 01:28:32 +0700

    3. DataTable + get API

--- lab06_rework/src/components/DataTable.jsx
+++ lab06_rework/src/components/DataTable.jsx
@@ -1,8 +1,52 @@
-export default function DataTable() {
-    return (
-        <div className="w-full h-64 bg-white border-2 border-dashed border-gray-400 rounded-lg flex items-center justify-center text-gray-500">
-            {/* Đây là khung giả để demo layout */}
-            Placeholder cho bảng dữ liệu (DataTable)
-        </div>
-    );
-}
+import React, { useEffect, useState } from 'react';
+ import DataTableBase from 'react-data-table-component';
+ 
+ export default function DataTable() {
+   const columns = [
+     {
+       name: 'CUSTOMER NAME',
+       selector: row => row.customer_name,
+       sortable: true,
+     },
+     {
+       name: 'COMPANY',
+       selector: row => row.company,
+       sortable: true,
+     },
+     {
+       name: 'ORDER VALUE',
+       selector: row => `$${row.order_value.toLocaleString()}`,
+       sortable: true,
+     },
+     {
+       name: 'ORDER DATE',
+       selector: row => row.order_date,
+       sortable: true,
+     },
+     {
+       name: 'STATUS',
+       selector: row => row.status,
+       sortable: true,
+     },
+   ];
+ 
+   const [users, setUsers] = useState([])
+   useEffect(() =>{
+     fetch('http://localhost:3000/user_data')
+         .then((r) => r.json())
+         .then((d) => setUsers(d))
+   },[])
+ 
+   return (
+     <div className="p-4">
+       <DataTableBase
+         title="Order Management"
+         columns={columns}
+         data={users}
+         pagination
+         highlightOnHover
+         selectableRows
+       />
+     </div>
+   );
+ }
