8a38b5be7c3718b8ba1e4005bc499e06bc878065
HaoLe2710 <lengochao123.27102004@gmail.com>
2025-04-20 01:22:46 +0700

    2. Get API Overview with Json Server

--- lab06_rework/src/components/NewCustomerCard.jsx
+++ lab06_rework/src/components/NewCustomerCard.jsx
@@ -1,12 +1,33 @@
-export default function NewCustomerTag(){
-    
-    return(
-        <div className="bg-blue-200 rounded-xl p-5 grid grid-cols-4">
-            <div className="flex flex-col col-span-3">
-                <p>New customer</p>
-                <p>32218</p>
-                <p>5.39% period at change</p>
-            </div>
-        </div>
-    )
-}
+import { useEffect, useState } from "react";
+ 
+ export default function NewCustomerTag(){
+ 
+     const [data, setData] = useState(null)
+     
+     useEffect(()=>{
+         fetch("http://localhost:3000/profit")
+             .then((r) => r.json())
+             .then((d) => {
+                 setData(d)
+             })
+             .catch((error) => console.error("Error fetching data:", error));
+     },[])
+ 
+     
+ 
+     if (!data) {
+         return <div>Loading...</div>  // Thêm thông báo loading khi data chưa có
+     }
+ 
+     const new_customer = data[0]
+ 
+     return(
+         <div className="bg-green-200 rounded-xl p-5 grid grid-cols-4">
+             <div className="flex flex-col col-span-3">
+                 <p>New customer</p>
+                 <p>{new_customer.value}</p>
+                 <p>{(new_customer.percented * 100).toFixed(2)}% period at change</p>
+             </div>
+         </div>
+     )
+ }
--- lab06_rework/src/components/ProfitTag.jsx
+++ lab06_rework/src/components/ProfitTag.jsx
@@ -1,10 +1,32 @@
+import { useEffect, useState } from "react";
+
 export default function ProfitTag(){
+
+    const [data, setData] = useState(null)
+    
+    useEffect(()=>{
+        fetch("http://localhost:3000/profit")
+            .then((r) => r.json())
+            .then((d) => {
+                setData(d)
+            })
+            .catch((error) => console.error("Error fetching data:", error));
+    },[])
+
+    
+
+    if (!data) {
+        return <div>Loading...</div>  // Thêm thông báo loading khi data chưa có
+    }
+
+    const profit = data[0]
+
     return(
-        <div className="bg-green-200 rounded-xl p-5 grid grid-col-4">
-            <div className="flex flex-col">
+        <div className="bg-green-200 rounded-xl p-5 grid grid-cols-4">
+            <div className="flex flex-col col-span-3">
                 <p>Profit</p>
-                <p>$32,218</p>
-                <p>5.39% period at change</p>
+                <p>${profit.value}</p>
+                <p>{(profit.percented * 100).toFixed(2)}% period at change</p>
             </div>
         </div>
     )
