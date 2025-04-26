ae81d3f8f17a3da533deb0ddfb4c6613a9a6f1da
HaoLe2710 <lengochao123.27102004@gmail.com>
2025-04-26 11:30:51 +0700

    0. Counter with Redux and Redux Toolkit

--- lab08/src/reduxToolkit/store.js
+++ lab08/src/reduxToolkit/store.js
@@ -1,6 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../reduxToolkit/features/counterSlice';
+import counterReducer from './features/counterSlice';
 
 export const store = configureStore({
   reducer: counterReducer
 });
+
+export default store
